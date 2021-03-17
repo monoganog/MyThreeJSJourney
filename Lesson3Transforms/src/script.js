2f98fed92e34b222affd1f8770b51a22b7cb94bc
Ben <monoganog@gmail.com>
2021-03-17 19:53:41 +1100

    Lesson3 Complete

--- Lesson3Transforms/src/script.js
+++ Lesson3Transforms/src/script.js
@@ -15,6 +15,52 @@ const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
+
+/**
+ * Objects
+ */
+const group = new THREE.Group()
+group.scale.y = 2
+group.rotation.y = 0.2
+scene.add(group)
+
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+)
+cube1.position.x = - 1.5
+group.add(cube1)
+
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+)
+cube2.position.x = 0
+group.add(cube2)
+
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+)
+cube3.position.x = 1.5
+group.add(cube3)
+
+mesh.position.x = 0.7
+mesh.position.y = - 0.6
+mesh.position.z = 1
+mesh.scale.x = 2
+mesh.scale.y = 0.25
+mesh.scale.z = 0.5
+mesh.rotation.x = Math.PI * 0.25
+mesh.rotation.y = Math.PI * 0.25
+
+
+/**
+ * Axes Helper
+ */
+ const axesHelper = new THREE.AxesHelper(2)
+ scene.add(axesHelper)
+
 /**
  * Sizes
  */
@@ -30,6 +76,7 @@ const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
 scene.add(camera)
 
+camera.lookAt(new THREE.Vector3(0, - 1, 0))
 /**
  * Renderer
  */
